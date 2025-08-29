712666f4ee5bb4f9ed1a89418bf103adcf2910cf
ACT-STUDENT <ACT-STUDENT@LAB3-PC29>
2025-08-29 14:41:18 +0800

    hehehe

---
+++ Yagonia_COConstructorUserInput/Yagonia_COConstructorUserInput/Phone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yagonia_COConstructorUserInput
+{
+    internal class Phone
+    {
+        //Declaring fields
+        public string brand;
+        public string model;
+        public int storage;
+
+        public Phone(string aBrand, string amodel, int aStorage)    //Parameterized Constructors
+        {
+            brand = aBrand;
+            model = amodel;
+            storage = aStorage;
+        }
+
+        //Method to display the book details
+        public void DisplayPhoneDetails()
+        {
+            Console.Clear();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("============= Here are the Phone details of your enlisted favorite phones =============\n");
+            Console.WriteLine("Phone Brand: \t" + brand);
+            Console.WriteLine("Phone Models: \t" + model);
+            Console.WriteLine("Phone Storage: \t" + storage);
+        }
+    }
+}
---
+++ Yagonia_COConstructorUserInput/Yagonia_COConstructorUserInput/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yagonia_COConstructorUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //Jack Anthony D. Yagonia
+        //IT306
+        //EXER_03: User Input with Constructor
+
+        Main:
+
+            Console.Clear();
+            //Will ask the user to input values
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("This program will ask you about your favorite phone:\n");
+            Console.Write("Enter phone brand:\t");
+            string aBrand = Console.ReadLine();
+            Console.Write("Enter phone model:\t");
+            string aModel = Console.ReadLine();
+            Console.Write("Enter phone storage:\t");
+            int aStorage = int.Parse(Console.ReadLine());
+
+            //Instantiation of a new object
+            Phone phone1 = new Phone(aBrand, aModel, aStorage);
+
+            //Call the method to display
+            phone1.DisplayPhoneDetails();
+
+            //Input another phone details
+            Console.Write("\nDo you want to have another entry(y/n)?: ");
+            char ans = Convert.ToChar(Console.ReadLine());
+
+            if (ans == 'y')
+            {
+                goto Main;
+            }
+            else if (ans == 'n')
+            {
+                return;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
